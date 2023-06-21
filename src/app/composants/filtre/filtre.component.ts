d51371517e8c3178d5665ed0149490fd56ab7a7e
Khouloud <khouloudfrikha@gmail.com>
2023-06-21 23:25:02 +0100

    modif filtre result

--- src/app/composants/filtre/filtre.component.ts
+++ src/app/composants/filtre/filtre.component.ts
@@ -2,13 +2,7 @@ import { Component, OnInit } from "@angular/core";
 import axios from "axios";
 import { Router } from "@angular/router";
 
-import {
-  FormBuilder,
-  FormGroup,
-  FormArray,
-  FormControl,
-  Validators,
-} from "@angular/forms";
+import { FormBuilder, FormGroup, FormArray, FormControl } from "@angular/forms";
 
 @Component({
   selector: "app-filtre",
@@ -22,27 +16,45 @@ export class FiltreComponent implements OnInit {
   weeks = [];
 
   form: FormGroup;
-  /* Hobbies: Array<any> =[
-    {name: 'Cricket', value:'Cricket'},
-    {name: 'TV', value:'TV'},
-  ]; */
-  constructor(private fb: FormBuilder, private router: Router) {
-    this.form = this.fb.group({
-      //checkArray: this.fb.array([]),
-      //valeur par defaut false
-    });
-  }
+
+  constructor(private fb: FormBuilder, private router: Router) {}
 
   ngOnInit(): void {
     this.getdivision();
     this.getprofit();
     this.getorganisation();
     this.getweek();
+
+    this.form = this.fb.group({
+      week_year_25_2023: new FormControl(),
+      week_year_24_2023: new FormControl(),
+      week_year_23_2023: new FormControl(),
+      week_year_22_2023: new FormControl(),
+      week_year_21_2023: new FormControl(),
+      week_year_20_2023: new FormControl(),
+      week_year_19_2023: new FormControl(),
+      week_year_18_2023: new FormControl(),
+      week_year_17_2023: new FormControl(),
+      week_year_16_2023: new FormControl(),
+      week_year_15_2023: new FormControl(),
+      week_year_14_2023: new FormControl(),
+      division_2000: new FormControl(),
+      division_2010: new FormControl(),
+      division_2020: new FormControl(),
+      division_2030: new FormControl(),
+      division_2040: new FormControl(),
+      division_2091: new FormControl(),
+      division_2092: new FormControl(),
+      division_2110: new FormControl(),
+      division_2200: new FormControl(),
+      division_2300: new FormControl(),
+      division_2320: new FormControl(),
+      division_2400: new FormControl(),
+    });
   }
 
   getdivision() {
     axios
-      // .get("http://127.0.0.1:8000/division/?format=json")
       .get("http://127.0.0.1:8000/filtre/division/")
       .then((res) => {
         console.log(res.data);
@@ -89,27 +101,88 @@ export class FiltreComponent implements OnInit {
       });
   }
 
-  onCkeckboxChange(e: any) {
-    const checkArray: FormArray = this.form.get("checkArray") as FormArray;
-  }
-  submitForm() {
-    console.log(this.form.value);
-  }
-
   onSubmit() {
     console.log("hello message");
-    //this.router.navigate(["/home/show"]);
-  }
 
-  // filterDivision() {
-  //   axios
-  //     .get("http:/127.0.0.1:8000/filtre/result/")
-  //     .then((res) => {
-  //       console.log(res.data);
-  //       this.divisions = res.data.Results;
-  //     })
-  //     .catch((err) => {
-  //       console.log(err);
-  //     });
-  // }
+    const week_year: string[] = [];
+
+    if (this.form.controls["week_year_25_2023"].value == true) {
+      week_year.push("25_2023");
+    }
+    if (this.form.controls["week_year_24_2023"].value == true) {
+      week_year.push("24_2023");
+    }
+    if (this.form.controls["week_year_23_2023"].value == true) {
+      week_year.push("23_2023");
+    }
+    if (this.form.controls["week_year_22_2023"].value == true) {
+      week_year.push("22_2023");
+    }
+    if (this.form.controls["week_year_21_2023"].value == true) {
+      week_year.push("21_2023");
+    }
+    if (this.form.controls["week_year_20_2023"].value == true) {
+      week_year.push("20_2023");
+    }
+    if (this.form.controls["week_year_19_2023"].value == true) {
+      week_year.push("19_2023");
+    }
+    if (this.form.controls["week_year_18_2023"].value == true) {
+      week_year.push("18_2023");
+    }
+    if (this.form.controls["week_year_17_2023"].value == true) {
+      week_year.push("17_2023");
+    }
+    if (this.form.controls["week_year_16_2023"].value == true) {
+      week_year.push("16_2023");
+    }
+    if (this.form.controls["week_year_15_2023"].value == true) {
+      week_year.push("15_2023");
+    }
+    if (this.form.controls["week_year_14_2023"].value == true) {
+      week_year.push("14_2023");
+    }
+
+    const divisions: string[] = [];
+
+    if (this.form.controls["division_2000"].value == true) {
+      divisions.push("2000");
+    }
+    if (this.form.controls["division_2010"].value == true) {
+      divisions.push("2010");
+    }
+    if (this.form.controls["division_2020"].value == true) {
+      divisions.push("2020");
+    }
+    if (this.form.controls["division_2030"].value == true) {
+      divisions.push("2030");
+    }
+    if (this.form.controls["division_2040"].value == true) {
+      divisions.push("2040");
+    }
+    if (this.form.controls["division_2091"].value == true) {
+      divisions.push("2091");
+    }
+    if (this.form.controls["division_2092"].value == true) {
+      divisions.push("2092");
+    }
+    if (this.form.controls["division_2110"].value == true) {
+      divisions.push("2110");
+    }
+    if (this.form.controls["division_2200"].value == true) {
+      divisions.push("2200");
+    }
+    if (this.form.controls["division_2300"].value == true) {
+      divisions.push("2300");
+    }
+    if (this.form.controls["division_2320"].value == true) {
+      divisions.push("2320");
+    }
+    if (this.form.controls["division_2400"].value == true) {
+      divisions.push("2400");
+    }
+    console.log(week_year);
+    console.log(divisions);
+    this.router.navigate(["/home/show"]);
+  }
 }
